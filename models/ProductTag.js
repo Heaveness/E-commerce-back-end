f1e52b2fcc38fd30535af6b480031d312e76b4ce
Heaveness <vindictuslbp@gmail.com>
2023-06-03 19:12:55 -0400

    Added code into models/index.js to define the models for category, product, tag, and product tag for structure and relationships for database tables. Also improved the indentation for all of the code.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -5,16 +5,17 @@ const sequelize = require('../config/connection');
 class ProductTag extends Model {}
 
 ProductTag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product_tag',
-  }
+	{
+		// define columns
+		
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'product_tag',
+	}
 );
 
 module.exports = ProductTag;
--- models/Tag.js
+++ models/Tag.js
@@ -5,16 +5,17 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
+	{
+		// define columns
+		
+	},
+	{
+		sequelize,
+		timestamps: false,
+		freezeTableName: true,
+		underscored: true,
+		modelName: 'tag',
+	}
 );
 
 module.exports = Tag;
