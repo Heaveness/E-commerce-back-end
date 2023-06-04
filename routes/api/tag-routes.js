e04797b1c75581da087898cf9e488be1284f7f98
Heaveness <vindictuslbp@gmail.com>
2023-06-04 14:17:02 -0400

    Added code to the tag-routes.js to create necessary route handlers for each product model.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,44 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-	// find all tags
-	// be sure to include its associated Product data
-
+router.get('/tags', async (req, res) => {
+	try {
+	  	const tags = await Tag.findAll({
+			include: [{ model: Product, through: ProductTag }],
+	  	});
+	  	res.status(200).json(tags);
+	} catch (err) {
+	  	res.status(500).json(err);
+	}
 });
 
-router.get('/:id', (req, res) => {
-	// find a single tag by its `id`
-	// be sure to include its associated Product data
-
+router.get('/tags/:id', async (req, res) => {
+	try {
+		const tag = await Tag.findByPk(req.params.id, {
+			include: [{ model: Product, through: ProductTag }],
+		});
+		if (!tag) {
+			res.status(404).json({ message: 'No tag found with this id' });
+			return;
+		}
+		res.status(200).json(tag);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.post('/', (req, res) => {
-	// create a new tag
-
+router.post('/tags', async (req, res) => {
+	try {
+		const tag = await Tag.create(req.body);
+		res.status(200).json(tag);
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
 router.put('/:id', (req, res) => {
 	// update a tag's name by its `id` value
-	
+
 });
 
 router.delete('/:id', (req, res) => {
