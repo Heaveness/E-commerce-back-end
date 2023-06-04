47943c4f54505f1b0db640229dca8a62cacf115f
Heaveness <vindictuslbp@gmail.com>
2023-06-04 15:38:11 -0400

    Added and changed the code within product-routes.js to create new products as well as delete them.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -32,35 +32,22 @@ router.get('/products/:id', async (req, res) => {
   });
 
 // create new product
-router.post('/', (req, res) => {
-	/* req.body should look like this...
-		{
-		product_name: "Basketball",
-		price: 200.00,
-		stock: 3,
-		tagIds: [1, 2, 3, 4]
-		}
-	*/
-  Product.create(req.body)
-    .then((product) => {
-		// if there's product tags, we need to create pairings to bulk create in the ProductTag model
-		if (req.body.tagIds.length) {
+router.post('/products', async (req, res) => {
+	try {
+		const product = await Product.create(req.body);
+		if (req.body.tagIds && req.body.tagIds.length) {
 			const productTagIdArr = req.body.tagIds.map((tag_id) => {
 			return {
 				product_id: product.id,
 				tag_id,
 			};
 			});
-			return ProductTag.bulkCreate(productTagIdArr);
-		}
-		// if no product tags, just respond
-		res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-		console.log(err);
+			await ProductTag.bulkCreate(productTagIdArr);
+	  	}
+	  	res.status(200).json(product);
+	} catch (err) {
 		res.status(400).json(err);
-    });
+	}
 });
 
 // update product
@@ -108,9 +95,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-	// delete one product by its `id` value
-	
+router.delete('/products/:id', async (req, res) => {
+	try {
+		const deletedProduct = await Product.destroy({
+			where: { id: req.params.id },
+		});
+		if (deletedProduct <= 0) {
+			res.status(404).json({ message: 'No product found with this id' });
+			return;
+		}
+		res.status(200).json({ message: 'Product deleted successfully' });
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
