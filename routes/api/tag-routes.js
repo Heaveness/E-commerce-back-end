582f9cb538b238f1457274d69e7e2026e40fa093
Heaveness <vindictuslbp@gmail.com>
2023-06-04 14:54:58 -0400

    Added more code for route handling within tag-routes.js.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,14 +38,38 @@ router.post('/tags', async (req, res) => {
 	}
 });
 
-router.put('/:id', (req, res) => {
-	// update a tag's name by its `id` value
-
+router.put('/tags/:id', async (req, res) => {
+	try {
+		const updatedTag = await Tag.update(req.body, {
+			where: {
+			id: req.params.id,
+			},
+		});
+		if (!updatedTag[0]) {
+			res.status(404).json({ message: 'No tag found with this id' });
+			return;
+		}
+		res.status(200).json({ message: 'Tag updated successfully' });
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-router.delete('/:id', (req, res) => {
-	// delete on tag by its `id` value
-	
+router.delete('/tags/:id', async (req, res) => {
+	try {
+		const deletedTag = await Tag.destroy({
+			where: {
+			id: req.params.id,
+			},
+		});
+		if (!deletedTag) {
+			res.status(404).json({ message: 'No tag found with this id' });
+			return;
+		}
+		res.status(200).json({ message: 'Tag deleted successfully' });
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 module.exports = router;
