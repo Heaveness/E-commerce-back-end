fb4e51526fed15cc1e86cdc5eee2410156a0ae4a
Heaveness <vindictuslbp@gmail.com>
2023-06-10 15:38:41 -0400

    Fixed the category-routes returning 404 error because of an accident in the code regarding wrong code.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,64 +17,64 @@ router.get('/', async (req, res) => {
 	}
 });
 
-// GET a specific product by ID.
-router.get('/products/:id', async (req, res) => {
+// GET a specific category by ID.
+router.get('/:id', async (req, res) => {
 	try {
-		// Find a product by its ID and include its associated category and tags.
-		const product = await Product.findByPk(req.params.id, {
-			include: [{ model: Category }, { model: Tag, through: ProductTag }],
+		// Find a category by its ID and include its associated products.
+		const category = await Category.findByPk(req.params.id, {
+			include: [{ model: Product }],
 		});
-		if (!product) {
-			res.status(404).json({ message: 'No product found with this id' }); // Return a 404 status if the product is not found.
+		if (!category) {
+			res.status(404).json({ message: 'No category found with this id' }); // Return a 404 status if the category is not found.
 			return;
 		}
-		res.status(200).json(product); // Return the product as a JSON response.
+		res.status(200).json(category); // Return the category as a JSON response.
 	} catch (err) {
 		res.status(500).json(err); // Return an error if an exception occurs.
 	}
 });
 
-// Create a new product.
-router.post('/products', async (req, res) => {
+// Create a new category.
+router.post('/', async (req, res) => {
 	try {
-		const productData = await Product.create(req.body); // Create a new product with the provided data.
-		res.status(201).json(productData); // Return the newly created product as a JSON response with a status of 201 (Created).
+		const categoryData = await Category.create(req.body); // Create a new category with the provided data.
+		res.status(201).json(categoryData); // Return the newly created category as a JSON response with a status of 201 (Created).
 	} catch (err) {
 		res.status(400).json(err); // Return an error if the request is invalid or missing required data.
 	}
 });
 
-// Update a product by ID.
+// Update a category by ID.
 router.put('/:id', async (req, res) => {
 	try {
-		const productData = await Product.update(req.body, {
+		const categoryData = await Category.update(req.body, {
 			where: {
 				id: req.params.id,
 			},
 		});
-		if (!productData[0]) {
-			res.status(404).json({ message: 'No product found with this id' }); // Return a 404 status if the product is not found.
+		if (!categoryData[0]) {
+			res.status(404).json({ message: 'No category found with this id' }); // Return a 404 status if the category is not found.
 			return;
 		}
-		res.status(200).json({ message: 'Product updated successfully' }); // Return a success message as a JSON response.
+		res.status(200).json({ message: 'Category updated successfully' }); // Return a success message as a JSON response.
 	} catch (err) {
 		res.status(400).json(err); // Return an error if the request is invalid or missing required data.
 	}
 });
 
-// Delete a product by ID.
+// Delete a category by ID.
 router.delete('/:id', async (req, res) => {
 	try {
-		const productData = await Product.destroy({
+		const categoryData = await Category.destroy({
 			where: {
 				id: req.params.id,
 			},
 		});
-		if (!productData) {
-			res.status(404).json({ message: 'No product found with this id' }); // Return a 404 status if the product is not found.
+		if (!categoryData) {
+			res.status(404).json({ message: 'No category found with this id' }); // Return a 404 status if the category is not found.
 			return;
 		}
-		res.status(200).json({ message: 'Product deleted successfully' }); // Return a success message as a JSON response.
+		res.status(200).json({ message: 'Category deleted successfully' }); // Return a success message as a JSON response.
 	} catch (err) {
 		res.status(500).json(err); // Return an error if an exception occurs.
 	}
